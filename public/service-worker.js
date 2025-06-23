aae5e061664625752730be6e90f36e4836fd7b65
SanAppsMano <manosandeslimafilho@gmail.com>
2025-06-23 13:31:29 -0300

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sanoptics-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  '../css/styles.css',
+  '../js/app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
